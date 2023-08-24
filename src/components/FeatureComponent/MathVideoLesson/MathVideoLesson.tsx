2f6e03ad5d4040ec2a6d3df87cb1f076e82a3deb
Vipul <vipul.soni@carveniche.com>
2023-08-24 18:28:50 +0530

    made some stylling changes

--- src/components/FeatureComponent/MathVideoLesson/MathVideoLesson.tsx
+++ src/components/FeatureComponent/MathVideoLesson/MathVideoLesson.tsx
@@ -16,14 +16,27 @@ export default function MathVideoLesson() {
     (state: RootState) => state.videoCallTokenData
   );
 
+  const { videoPlayState } = useSelector(
+    (state: RootState) => state.liveClassDetails
+  );
+
   const { extraParams } = activeTabArray[currentSelectedIndex];
-  console.log("extraParams ", extraParams);
 
   useEffect(() => {
-    return () => {
-      handleDataTrack(false);
-    };
-  }, []);
+    console.log("videoPlayState", videoPlayState);
+    if (videoPlayState) {
+      handleVideoAction("play");
+    } else {
+      handleVideoAction("pause");
+    }
+  }, [videoPlayState]);
+
+  // useEffect(() => {
+  //   return () => {
+  //     console.log("MathVideoLesson component");
+  //     // handleDataTrack(false);
+  //   };
+  // }, []);
 
   const handleDataTrack = (state: boolean) => {
     const [localDataTrackPublication] = [
@@ -47,49 +60,69 @@ export default function MathVideoLesson() {
       if (action === "play") {
         videoRef.current.play();
         setVideoPlaying(true);
-        handleDataTrack(true);
+        if (isTutor({ identity: String(role_name) })) {
+          console.log("send datatrack from play video as identity is tutor");
+          handleDataTrack(true);
+        }
       } else if (action === "pause") {
         videoRef.current.pause();
-        handleDataTrack(false);
         setVideoPlaying(false);
+        if (isTutor({ identity: String(role_name) })) {
+          console.log("send datatrack from play video as identity is tutor");
+          handleDataTrack(false);
+        }
       }
     }
   };
 
   return (
     <div className="flex flex-col w-full h-full justify-center items-center ">
-      <div className="flex-1/4 justify-center items-center">
+      <div className="flex flex-row justify-center items-center h-[30px] w-[30px]">
         {isTutor({ identity: String(role_name) }) ? (
-          <button
-            className={`bg-${videoPlaying ? "red" : "blue"}-500 hover:bg-${
-              videoPlaying ? "red" : "blue"
-            }-700 text-white font-bold py-2 px-4 rounded mt-16`}
-            onClick={() => {
-              if (videoPlaying) {
-                handleVideoAction("pause");
-              } else {
-                handleVideoAction("play");
-              }
-            }}
-          >
-            {videoPlaying ? "Pause" : "Play"}
-          </button>
+          <>
+            <button
+              style={{
+                background: videoPlaying ? "red" : "blue",
+                color: "white",
+                padding: 10,
+                borderRadius: "5px",
+                fontSize: "bold",
+                marginTop: "10px",
+              }}
+              onClick={() => {
+                if (videoPlaying) {
+                  handleVideoAction("pause");
+                } else {
+                  handleVideoAction("play");
+                }
+              }}
+            >
+              {videoPlaying ? "Pause" : "Play"}
+            </button>
+          </>
         ) : null}
       </div>
       {videoPlaying && isTutor({ identity: String(role_name) }) && (
-        <div>
-          <h5>
+        <div className="flex w-full h-[30px] justify-center items-center mt-5">
+          <h1>
             All participants are muted during video play. Pausing the video will
             unmute the participants
-          </h5>
+          </h1>
         </div>
       )}
-      <div className="flex-3/4 justify-center items-center  w-55">
+
+      <div
+        style={{
+          height: "calc(100% - 50px)",
+          marginTop: "25px",
+        }}
+        className="justify-center items-center"
+      >
         <video
           ref={videoRef}
           style={{
-            width: "86%",
-            height: "95%",
+            width: "100%",
+            height: "100%",
             objectFit: "contain",
           }}
           playsInline
