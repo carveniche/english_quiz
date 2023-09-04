bf2446bb01283c6bae37fc7904d3314f738b167f
anishtiwari09 <aneeshtiwari24@gmail.com>
2023-09-04 12:09:38 +0530

    commit

--- src/components/FeatureComponent/Mathzone/Teacher/ViewQuestionAtMiddle.jsx
+++ src/components/FeatureComponent/Mathzone/Teacher/ViewQuestionAtMiddle.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from "react";
-import { ViewStatusContext } from "../Mathzone";
+import { ViewStatusContext } from "../mathzone";
 import TeacherViewEachResponse from "./TeacherViewEachResponse";
 import { ValidationContext } from "../MainOnlineQuiz/MainOnlineQuizPage";
 
