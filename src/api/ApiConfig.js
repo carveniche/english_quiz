dd0c99add2f8309a3571119fec5b5bf792d8b084
Vipul <vipul.soni@carveniche.com>
2023-09-28 11:39:53 +0530

    change base url

--- src/api/ApiConfig.js
+++ src/api/ApiConfig.js
@@ -1,5 +1,7 @@
 //const BaseUrl = "https://begalileo.com/";
 
-const BaseUrl = "https://testserver.begalileo.com/";
+//const BaseUrl = "https://testserver.begalileo.com/";
+
+const BaseUrl = "//testserver.begalileo.com//";
 
 export default BaseUrl;
