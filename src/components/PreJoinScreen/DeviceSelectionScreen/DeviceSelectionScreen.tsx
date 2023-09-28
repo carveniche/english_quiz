c8759b2632888e7d346789f7053e2719fbef4142
Vipul <vipul.soni@carveniche.com>
2023-09-28 19:21:10 +0530

    VT.2.8 pushed

--- src/components/PreJoinScreen/DeviceSelectionScreen/DeviceSelectionScreen.tsx
+++ src/components/PreJoinScreen/DeviceSelectionScreen/DeviceSelectionScreen.tsx
@@ -323,7 +323,7 @@ export default function DeviceSelectionScreen({
                     cursor: "none",
                   }}
                 >
-                  T.2.7
+                  T.2.8
                 </Button>
                 <Button
                   variant="contained"
