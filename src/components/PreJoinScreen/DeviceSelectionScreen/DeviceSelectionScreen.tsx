33be9e12f45ca0ded755c060b6aaec1f18d49225
Vipul <vipul.soni@carveniche.com>
2023-09-28 11:40:13 +0530

    change version

--- src/components/PreJoinScreen/DeviceSelectionScreen/DeviceSelectionScreen.tsx
+++ src/components/PreJoinScreen/DeviceSelectionScreen/DeviceSelectionScreen.tsx
@@ -323,7 +323,7 @@ export default function DeviceSelectionScreen({
                     cursor: "none",
                   }}
                 >
-                  T.2.5
+                  T.2.6
                 </Button>
                 <Button
                   variant="contained"
