1a979a043fff3a2d1bb8cae35f6d3cd4104194be
Vipul <vipul.soni@carveniche.com>
2023-10-18 13:32:35 +0530

    change baseUrl to testserver

--- src/components/PreJoinScreen/DeviceSelectionScreen/DeviceSelectionScreen.tsx
+++ src/components/PreJoinScreen/DeviceSelectionScreen/DeviceSelectionScreen.tsx
@@ -323,7 +323,7 @@ export default function DeviceSelectionScreen({
                     cursor: "none",
                   }}
                 >
-                  T.2.21
+                  T.2.22
                 </Button>
                 <Button
                   variant="contained"
