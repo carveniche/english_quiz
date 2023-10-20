36333404ccc70a98b73e26b08324bfeebc5dfc9b
Vipul <vipul.soni@carveniche.com>
2023-10-20 10:46:27 +0530

    fix import for fiveStarLottie

--- src/components/PreJoinScreen/DeviceSelectionScreen/DeviceSelectionScreen.tsx
+++ src/components/PreJoinScreen/DeviceSelectionScreen/DeviceSelectionScreen.tsx
@@ -323,7 +323,7 @@ export default function DeviceSelectionScreen({
                     cursor: "none",
                   }}
                 >
-                  T.2.26
+                  T.2.27
                 </Button>
                 <Button
                   variant="contained"
