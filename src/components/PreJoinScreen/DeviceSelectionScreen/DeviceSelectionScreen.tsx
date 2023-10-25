feac40093c2c57a74611965a930d32523af702a5
Vipul <vipul.soni@carveniche.com>
2023-10-25 15:31:06 +0530

    change test version

--- src/components/PreJoinScreen/DeviceSelectionScreen/DeviceSelectionScreen.tsx
+++ src/components/PreJoinScreen/DeviceSelectionScreen/DeviceSelectionScreen.tsx
@@ -323,7 +323,7 @@ export default function DeviceSelectionScreen({
                     cursor: "none",
                   }}
                 >
-                  T.2.30
+                  T.2.31
                 </Button>
                 <Button
                   variant="contained"
