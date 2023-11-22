8834f1c2585852537ca11e56e58d1700de326a8f
anishtiwari09 <aneeshtiwari24@gmail.com>
2023-11-22 12:24:04 +0530

    change base url

--- src/components/PreJoinScreen/DeviceSelectionScreen/DeviceSelectionScreen.tsx
+++ src/components/PreJoinScreen/DeviceSelectionScreen/DeviceSelectionScreen.tsx
@@ -323,7 +323,7 @@ export default function DeviceSelectionScreen({
                     cursor: "none",
                   }}
                 >
-                  T.2.41
+                  T.2.42
                 </Button>
                 <Button
                   variant="contained"
