29ec94f40c5ef5e2ab89ba4a14d1a958d4ae91eb
Vipul <vipul.soni@carveniche.com>
2023-11-24 14:59:36 +0530

    change todayIcon

--- src/components/PreJoinScreen/DeviceSelectionScreen/DeviceSelectionScreen.tsx
+++ src/components/PreJoinScreen/DeviceSelectionScreen/DeviceSelectionScreen.tsx
@@ -323,7 +323,7 @@ export default function DeviceSelectionScreen({
                     cursor: "none",
                   }}
                 >
-                  T.2.47
+                  T.2.48
                 </Button>
                 <Button
                   variant="contained"
