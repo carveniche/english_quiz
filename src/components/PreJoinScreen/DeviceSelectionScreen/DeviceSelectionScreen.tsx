8adf0e9aed40a455f43a43aaf2ffcbd053478a6b
anishtiwari09 <aneeshtiwari24@gmail.com>
2023-11-26 23:38:08 +0530

    T.2.50

--- src/components/PreJoinScreen/DeviceSelectionScreen/DeviceSelectionScreen.tsx
+++ src/components/PreJoinScreen/DeviceSelectionScreen/DeviceSelectionScreen.tsx
@@ -323,7 +323,7 @@ export default function DeviceSelectionScreen({
                     cursor: "none",
                   }}
                 >
-                  T.2.49
+                  T.2.50
                 </Button>
                 <Button
                   variant="contained"
