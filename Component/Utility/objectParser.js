e54a748954e5aa0bf01c759a020ffe5f2a75e17f
anishtiwari09 <aneeshtiwari24@gmail.com>
2023-12-26 10:46:58 +0530

    add font style
    ;

--- Component/Utility/objectParser.js
+++ Component/Utility/objectParser.js
@@ -1,17 +1,33 @@
 const objectParser = (item, index) => {
   let value = "";
   if (item?.node === "text") {
+    const { styles } = item;
+    styles = styles || item?.style || [];
+    if (styles.length) {
+      let cssStyle = {};
+      for (let style of styles) {
+        if (style.textDecoration) {
+          cssStyle.textDecoration = style.textDecoration;
+        } else if (style?.fontWeight) {
+          cssStyle.fontWeight = style.fontWeight;
+        } else if (style?.color) {
+          cssStyle.color = style.color;
+        } else if (style?.fontStyle) {
+          cssStyle.fontStyle = style.fontStyle;
+        }
+      }
+    }
     value = <>{item?.value}</>;
   } else if (item?.node === "img") {
     value = (
       <div>
-        <img src={item?.value} />
+        <img src={item?.value} style={{ textDecoration: "under" }} />
       </div>
     );
   } else if (item?.node === "audio") {
     value = <>Audio symbol</>;
   }
-  if (item?.inNewLine) return <div>{value}</div>;
+  if (item?.inNewLine) return <div style={{ marginTop: 4 }}>{value}</div>;
   return value;
 };
 export default objectParser;
