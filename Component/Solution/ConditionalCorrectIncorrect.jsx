31b95fb29fb43e6e6d8fa522150a17eaf2adb4d9
Your Name <anish@carveniche.com>
2024-01-29 18:39:53 +0530

    commit

--- Component/Solution/ConditionalCorrectIncorrect.jsx
+++ Component/Solution/ConditionalCorrectIncorrect.jsx
@@ -1,11 +1,13 @@
 import React, { useContext, useEffect } from "react";
 import styles from "./Solution.module.css";
 import QuestionTypeResponse, {
+  SolutionForDragDrop,
   SolutionForReordering,
 } from "./utility/QuestionTypeResponse";
 import { ValidationContext } from "../QuizPage";
 export default function ConditionalCorrectIncorrect({ obj, question_type }) {
   let reordering = ["Horizontal Ordering", "Vertical Ordering"];
+  let dragDrop=["Math the Following"]
   const { setDisabledQuestion } = useContext(ValidationContext);
   const writingGpt = ["Writing ChatGpt"];
   useEffect(() => {
@@ -17,7 +19,7 @@ export default function ConditionalCorrectIncorrect({ obj, question_type }) {
         <SolutionForReordering obj={obj} question_type={question_type} />
       ) : writingGpt?.includes(question_type) ? (
         ""
-      ) : (
+      ) : dragDrop.includes(question_type)?<SolutionForDragDrop obj={obj} question_type={question_type}/>:(
         <div className={`${styles.correctAnswer} ${styles.correctAnswer2}`}>
           <h6>The correct answer is:</h6>
           <QuestionTypeResponse question_type={question_type} obj={obj} />
