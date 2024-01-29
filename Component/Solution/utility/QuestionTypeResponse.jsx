68dfc3265c6a0c79c25b4b3ae33a697aedaa20a9
Your Name <anish@carveniche.com>
2024-01-29 18:40:12 +0530

    commit

--- Component/Solution/utility/QuestionTypeResponse.jsx
+++ Component/Solution/utility/QuestionTypeResponse.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import DragDrop from "../../QuizQuestion/reordering/Dragdrop";
 import styles from "../Solution.module.css";
+import DragDropSolution from "../../QuizQuestion/MatchTheFollowing/DragDropSolution";
 export default function QuestionTypeResponse({ obj, question_type }) {
   let questionData = JSON.parse(obj?.question_data);
   let choices = questionData?.choices || [];
@@ -39,6 +40,25 @@ export function SolutionForReordering({ obj, question_type }) {
           direction={
             question_type == "Vertical Ordering" ? "vertical" : "horizontal"
           }
+        />
+      </div>
+    </>
+  );
+}
+
+export function SolutionForDragDrop({ obj, question_type }) {
+  let questionData = JSON.parse(obj?.question_data);
+  return (
+    <>
+      <div
+        className={`${styles.correctAnswer} ${styles.correctAnswer2}`}
+        style={{ display: "block" }}
+      >
+        <h6>The correct answer is:</h6>
+        <DragDropSolution
+          questionContent={questionData?.question_content
+            || []}
+          
         />
       </div>
     </>
