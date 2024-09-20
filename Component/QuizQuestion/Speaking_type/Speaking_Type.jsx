d38b7c7e3f94d873f25afd2fe6ec433eba7f1fc2
VinayakRNyk <vinayak@carveniche.com>
2024-09-20 16:38:58 +0530

    align issue fix

--- Component/QuizQuestion/Speaking_type/Speaking_Type.jsx
+++ Component/QuizQuestion/Speaking_type/Speaking_Type.jsx
@@ -1,23 +1,23 @@
 import React, { useState } from 'react'
 
 import styles from "../english_mathzone.module.css";
- // import objectParser from "../../Utility/objectParser";
+  import objectParser from "../../Utility/objectParser";
 import Recording_part from './Recording_part';
 import ResourceViewer from '../../CommonComponent/ResourceViewer';
 
 export default function Speaking_Type({ questionData,questionResponse }) {
-  const objectParser = (item, index) => {
-    let value = "";
-    if (item?.node === "text") {
-      value = <>{item?.value}</>;
-    } else if (item?.node === "img") {
-      value = <img src={item?.value} />;
-    } else if (item?.node === "audio") {
-      value = <>Audio symbol</>;
-    }
-    if (item?.inNewLine) return <div>{value}</div>;
-    return value;
-  };
+  // const objectParser = (item, index) => {
+  //   let value = "";
+  //   if (item?.node === "text") {
+  //     value = <>{item?.value}</>;
+  //   } else if (item?.node === "img") {
+  //     value = <img src={item?.value} />;
+  //   } else if (item?.node === "audio") {
+  //     value = <>Audio symbol</>;
+  //   }
+  //   if (item?.inNewLine) return <div>{value}</div>;
+  //   return value;
+  // };
   return (
     <div>
          <div>
