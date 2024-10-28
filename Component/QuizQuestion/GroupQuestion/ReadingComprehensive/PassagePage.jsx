6e448b4c69659069409c137a3045513f377ba4ca
Vinaygouda Meti <vinayagoud@carveniche.com>
2024-10-28 11:45:59 +0530

    formatting

--- Component/QuizQuestion/GroupQuestion/ReadingComprehensive/PassagePage.jsx
+++ Component/QuizQuestion/GroupQuestion/ReadingComprehensive/PassagePage.jsx
@@ -6,33 +6,30 @@ import NotificationModal from "./NotificationModal";
 export default function PassagePage({ groupData }) {
   const { handleShowQuestion } = useContext(GroupQuestionContext);
   const [currentPage, setCurrentPage] = useState(0);
-  const pageChangeRef=useRef(null)
+  const pageChangeRef = useRef(null);
   const handleChangePage = (val) => {
-    if(pageChangeRef.current)
-    return
+    if (pageChangeRef.current) return;
     setCurrentPage(currentPage + val);
-    let element =document.getElementsByClassName("english_vertical_bar")||[]
-    for(let item of element){
-      if(item)
-      item.style.opacity="0.4"
+    let element = document.getElementsByClassName("english_vertical_bar") || [];
+    for (let item of element) {
+      if (item) item.style.opacity = "0.4";
     }
-    pageChangeRef.current=setTimeout(()=>{
-      for(let item of element){
-        if(item)
-        item.style.opacity="1"
+    pageChangeRef.current = setTimeout(() => {
+      for (let item of element) {
+        if (item) item.style.opacity = "1";
       }
-      clearTimeout(pageChangeRef.current)
-      pageChangeRef.current=null
-    },4000)
+      clearTimeout(pageChangeRef.current);
+      pageChangeRef.current = null;
+    }, 4000);
   };
   const leftArrowBtn = useRef(null);
   const rightArrowBtn = useRef(null);
   window.leftArrowBtn = leftArrowBtn.current;
-  window.rightArrowBtn = rightArrowBtn.current
-  const [hideNotification,setHideNotification]=useState(false)
-  useEffect(()=>{
-return ()=>clearTimeout(pageChangeRef.current)
-  },[])
+  window.rightArrowBtn = rightArrowBtn.current;
+  const [hideNotification, setHideNotification] = useState(false);
+  useEffect(() => {
+    return () => clearTimeout(pageChangeRef.current);
+  }, []);
   return (
     <div
       style={{
@@ -42,47 +39,37 @@ return ()=>clearTimeout(pageChangeRef.current)
         margin: "auto",
       }}
     >
-      {!hideNotification&&<NotificationModal onClose={setHideNotification}/>}
-     {hideNotification&& <>
-        <Page passage={groupData[currentPage] || []} />
-        {currentPage > 0 && (
-          
-          // <button
-          //   onClick={() => handleChangePage(-1)}
-          //   className={`${styles.reading_comprehensive_btn} ${styles.prev_btn} react_passage_change_page_change_btn react_passage_left_btn`}
-          //   style={{ background: "initial", border: 0, cursor: "pointer" }}
-          //   ref={leftArrowBtn}
-          // >
-          //   <LeftArrow />
-          // </button>
-          <button class={`${styles.leftButton} ${styles.naviagationButton} ${styles.prev_btn} ${styles.reading_comprehensive_btn} react_passage_change_page_change_btn react_passage_left_btn`} onClick={() => handleChangePage(-1)}>
-               <i class={`${styles.fa} ${styles["fa-caret-left"]}`}></i>
-          </button>
-        )}
-        {currentPage + 1 < groupData.length ? (
-            <button class={`${styles.next_btn} ${styles.naviagationButton} ${styles.reading_comprehensive_btn} react_passage_change_page_change_btn react_passage_right_btn`} onClick={() => handleChangePage(1)}>
-            <i class={`${styles.fa} ${styles["fa-caret-right"]}`}></i>
-       </button>
-          // <button
-          //   onClick={() => handleChangePage(+1)}
-          //   className={`${styles.reading_comprehensive_btn} ${styles.next_btn} react_passage_change_page_change_btn react_passage_right_btn`}
-          //   ref={rightArrowBtn}
-          // >
-          //   <RightArrow />
-          // </button>
-        ) : (
-          // <button
-          //   className={`${styles.reading_comprehensive_btn} ${styles.next_btn} react_passage_change_page_change_btn react_passage_right_btn`}
-          //   onClick={handleShowQuestion}
-          //   ref={rightArrowBtn}
-          // >
-          //   <RightArrow />
-          // </button>
-          <button class={`${styles.next_btn} ${styles.naviagationButton} ${styles.reading_comprehensive_btn} react_passage_change_page_change_btn react_passage_right_btn`}  onClick={()=>{!pageChangeRef.current&&handleShowQuestion()}}>
-          <i class={`${styles.fa} ${styles["fa-caret-right"]}`}></i>
-     </button>
-        )}
-      </>}
+      {!hideNotification && <NotificationModal onClose={setHideNotification} />}
+      {hideNotification && (
+        <>
+          <Page passage={groupData[currentPage] || []} />
+          {currentPage > 0 && (
+            <button
+              className={`${styles.leftButton} ${styles.naviagationButton} ${styles.prev_btn} ${styles.reading_comprehensive_btn} react_passage_change_page_change_btn react_passage_left_btn`}
+              onClick={() => handleChangePage(-1)}
+            >
+              <i className={`${styles.fa} ${styles["fa-caret-left"]}`}></i>
+            </button>
+          )}
+          {currentPage + 1 < groupData.length ? (
+            <button
+              className={`${styles.next_btn} ${styles.naviagationButton} ${styles.reading_comprehensive_btn} react_passage_change_page_change_btn react_passage_right_btn`}
+              onClick={() => handleChangePage(1)}
+            >
+              <i className={`${styles.fa} ${styles["fa-caret-right"]}`}></i>
+            </button>
+          ) : (
+            <button
+              className={`${styles.next_btn} ${styles.naviagationButton} ${styles.reading_comprehensive_btn} react_passage_change_page_change_btn react_passage_right_btn`}
+              onClick={() => {
+                !pageChangeRef.current && handleShowQuestion();
+              }}
+            >
+              <i className={`${styles.fa} ${styles["fa-caret-right"]}`}></i>
+            </button>
+          )}
+        </>
+      )}
     </div>
   );
 }
